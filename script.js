3ba0d54dcbfc3e94936e636d205567ba5d832037
jurada <tokontoszkolne@gmail.com>
2022-08-29 19:05:51 +0200

    add a button that changes grid size

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const grid = document.querySelector(".grid")
 
+const btn= document.querySelector("#size-changer");
+
 function makeGrid(size) {
+    grid.rem
 
     for (let i = 0; i < size; i++) {
         let row = document.createElement("div");
@@ -23,4 +26,18 @@ function makeACell() {
     return cell;
 }
 
+function clearGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
 makeGrid(16);
+
+btn.addEventListener('click',()=>{
+    let size = prompt("please enter new size:");
+    if(0<size && size <100){
+        clearGrid();
+        makeGrid(size);
+    }
+})
