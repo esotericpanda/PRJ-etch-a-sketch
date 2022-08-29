e6069301fd7065d5c462bfd9e3c39cc413dfea7e
jurada <tokontoszkolne@gmail.com>
2022-08-29 18:49:43 +0200

    set up an effect where cells change color when hovered over

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-const grid=document.querySelector(".grid")
+const grid = document.querySelector(".grid")
 
-function makeGrid(size){
-    console.log("called")
-    for(let i=0; i<size; i++){
-        let row=document.createElement("div");
+function makeGrid(size) {
+
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
         row.classList.add("row");
-        for(let j=0;j<size;j++){
-            let cell=document.createElement("div");
-            cell.classList.add("cell");
+
+        for (let j = 0; j < size; j++) {
+            let cell = makeACell();
             row.appendChild(cell);
         }
         grid.appendChild(row);
     }
+}
+
+function makeACell() {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.addEventListener('mouseover',(e)=>{
+        e.target.style.backgroundColor='black';
+    });
+    return cell;
 }
 
 makeGrid(16);
